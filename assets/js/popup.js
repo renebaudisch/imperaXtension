0b653f6f84664ca559ccc09214e0dd8f36380b0e
René Baudisch <rene.baudisch@t-online.de>
2018-04-10 08:42:28 +0200

    working on beta

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,13 +1,32 @@
-/*let changeColor = document.getElementById('changeColor');
+let loginSubmit = document.getElementById('loginSubmit');
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get('imperaOnline', function(data) {
+    if (!data.user && !data.pass) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                // innerText does not let the attacker inject HTML elements.
+                document.getElementById("resp").innerText = xhr.responseText;
+            }
+        };
+        xhr.url = xhr.open("GET", "https://www.imperaonline.de/api/notifications/summary", true);
+        xhr.send();
+    } else {
+        document.querySelector('#loginContainer').style.display =" none";
+    }
 });
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.executeScript(
-        undefined,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-};*/
+loginSubmit.onclick = function() {
+    let user = document.getElementById('loginPass');
+    let pass = document.getElementById('loginUser');
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(data) {
+        if (xhr.readyState === 4) {
+            chrome.storage.imperaOnline.user = user.value;
+            chrome.storage.imperaOnline.pass = pass.value;
+            chrome.storage.imperaOnline.tocken = data.token;
+        }
+    };
+    xhr.url = xhr.open("POST", "https://www.imperaonline.de/api/Account/token?", true);
+    xhr.send();
+};
