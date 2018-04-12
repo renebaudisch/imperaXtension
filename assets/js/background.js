a60dd90d4cce824da804897b578f82af12be236b
René Baudisch <rene.baudisch@t-online.de>
2018-04-12 22:21:11 +0200

    only pushToken left...

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,16 +1,20 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({
-        imperaOnline: {
-            user: "",
-            pass: "",
-            auth: {},
-            gameCounter: 0,
-            gameList: []
-        }
-    });
-});
-
 var imperaXtension = {
+    summaryTimer: 0,
+    passToken: function(){
+        let imperaStorage = sessionStorage.impera;
+        if (imperaStorage) {
+            imperaStorage = JSON.parse(imperaStorage);
+            imperaStorage.auth_token = imperaXtension.auth.access_token;
+            imperaStorage.refresh_token = imperaXtension.auth.refresh_token;
+            imperaStorage.isLoggedIn = true;
+            imperaStorage.notifications = {
+                numberOfGames: imperaXtension.gameCounter,
+                numberOfMessages: imperaXtension.messageCounter
+            };
+            sessionStorage.setItem("impera", imperaStorage);
+        }
+
+    },
     login: function(user, pass) {
         imperaXtension.user = user;
         imperaXtension.pass = pass;
@@ -26,12 +30,16 @@ var imperaXtension = {
                         auth: imperaOnlineAuth
                     }
                 });
-                setTimeout(function() {
+                imperaXtension.loginTimer = setTimeout(function() {
+                    clearInterval(imperaXtension.summaryTimer);
                     imperaXtension.login(imperaXtension.user, imperaXtension.pass);
                 }, (imperaOnlineAuth.expires_in * 1000) - 1000);
-                frontend.document.querySelector('#loginContainer').style.display =" none";
+                if (window.frontend){
+                    frontend.document.querySelector('#loginContainer').style.display ="none";
+                    frontend.document.querySelector('#loggedInContainer').style.display ="block";
+                }
                 imperaXtension.getSummary();
-                setInterval(imperaXtension.getSummary, 5000);
+                imperaXtension.summaryTimer = setInterval(imperaXtension.getSummary, 5000);
             }
         };
         xhr.open("POST", "https://www.imperaonline.de/api/Account/token?", true);
@@ -44,25 +52,63 @@ var imperaXtension = {
     getSummary: function(){
         let xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                // innerText does not let the attacker inject HTML elements.
+            if (xhr.readyState === 4  && xhr.status === 200) {
                 let summary = JSON.parse(xhr.responseText);
                 imperaXtension.gameCounter = summary.numberOfGames;
                 imperaXtension.messageCounter = summary.numberOfMessages;
                 let logoNumber = (imperaXtension.gameCounter > 9) ? "10" : imperaXtension.gameCounter;
                 if (imperaXtension.gameCounter > 0) {
-                    frontend.document.getElementById("gameList").innerText = "Du bist in diesen " + imperaXtension.gameCounter + " Spielen am Zug:";
                     chrome.browserAction.setBadgeBackgroundColor({color:[255, 0, 0, 130]});
                     chrome.browserAction.setBadgeText({text: String(logoNumber)});
+                    if (window.frontend){
+                        imperaXtension.getGameList();
+                    }
                 } else {
-                    frontend.document.getElementById("gameList").innerText = "Du bist leider in keinem Spiel am Zug :(";
                     chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
                     chrome.browserAction.setBadgeText({text: ""});
+                    if (window.frontend){
+                        imperaXtension.gameList = [];
+                        frontend.document.getElementById("gameList").innerText = "Du bist leider in keinem Spiel am Zug :(";
+                    }
                 }
             }
         };
         xhr.open("GET", "https://www.imperaonline.de/api/notifications/summary", true);
         xhr.setRequestHeader('Authorization', imperaXtension.auth.token_type + ' ' + imperaXtension.auth.access_token);
         xhr.send();
+    },
+    getGameList: function() {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                imperaXtension.gameList = JSON.parse(xhr.responseText);
+                frontend.imperaXtension.renderGameList();
+            }
+        };
+        xhr.open("GET", "https://www.imperaonline.de/api/games/myturn", true);
+        xhr.setRequestHeader('Authorization', imperaXtension.auth.token_type + ' ' + imperaXtension.auth.access_token);
+        xhr.send();
+    },
+    logout: function() {
+        clearInterval(imperaXtension.summaryTimer);
+        chrome.storage.sync.set({
+            imperaOnline: {}
+        });
+        imperaXtension.gameList = [];
+        imperaXtension.gameCounter = 0;
+        imperaXtension.messageCounter = 0;
+        imperaXtension.user = "";
+        imperaXtension.pass = "";
+        imperaXtension.auth = {};
+
+        chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
+        chrome.browserAction.setBadgeText({text: ""});
     }
 };
+
+chrome.storage.sync.get('imperaOnline', function(data) {
+    data = data.imperaOnline;
+    if (data.user && data.pass) {
+        imperaXtension.login(data.user, data.pass);
+    }
+});
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,5 +1,34 @@
 let loginSubmit = document.getElementById('loginSubmit');
 
+var imperaXtension = {
+    humanDate: function msToTime(duration) {
+        var seconds = parseInt((duration/1000)%60)
+            , minutes = parseInt((duration/(1000*60))%60)
+            , hours = parseInt((duration/(1000*60*60))%24)
+            , days = parseInt((duration/(1000*60*60*24))%7);
+
+        days = (days === 0) ? "" : ((days === 1) ? days + " Tag" : days + " Tage");
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return days + " " + hours + ":" + minutes + ":" + seconds;
+    },
+    renderGameList: function() {
+        var gameList = document.createElement('div');
+        gameList.setAttribute('class', "gamelist");
+        gameList.innerHTML = "<div class='headline'>Du bist in diesen " + backend.imperaXtension.gameCounter + " Spielen am Zug:</div>";
+        for (var i = 0; i < backend.imperaXtension.gameList.length; i++) {
+            gameList.innerHTML += "<div><a target='_blank' href='https://www.imperaonline.de/play/" + backend.imperaXtension.gameList[i].id + "'>" +
+                "<div class='gameName'>" + backend.imperaXtension.gameList[i].name + "</div>" +
+                "<div class='gameTime'>" + imperaXtension.humanDate(backend.imperaXtension.gameList[i].timeoutSecondsLeft * 1000) + "</div>" +
+                "</a></div>";
+        }
+        document.getElementById('gameList').innerHTML = "";
+        document.getElementById('gameList').appendChild(gameList);
+    }
+
+};
 chrome.storage.sync.get('imperaOnline', function(data) {
     data = data.imperaOnline;
     if (data.user && data.pass) {
@@ -8,14 +37,16 @@ chrome.storage.sync.get('imperaOnline', function(data) {
             document.getElementById('loginPass').value = data.pass;
         } else {
             if (backend.imperaXtension.gameCounter > 0) {
-                document.getElementById("gameList").innerText = "Du bist in diesen " + backend.imperaXtension.gameCounter + " Spielen am Zug:";
+                imperaXtension.renderGameList();
             } else {
                 document.getElementById("gameList").innerText = "Du bist leider in keinem Spiel am Zug :(";
             }
-            document.querySelector('#loginContainer').style.display =" none";
+            document.querySelector('#loginContainer').style.display ="none";
+            document.querySelector('#loggedInContainer').style.display ="block";
         }
     } else {
-        document.querySelector('#loginContainer').style.display =" block";
+        document.querySelector('#loginContainer').style.display ="block";
+        document.querySelector('#loggedInContainer').style.display ="none";
     }
 });
 
@@ -26,4 +57,10 @@ loginSubmit.onclick = function() {
     var user = document.getElementById('loginName').value;
     var pass = document.getElementById('loginPass').value;
     backend.imperaXtension.login(user, pass);
+};
+
+logout.onclick = function() {
+    backend.imperaXtension.logout();
+    document.querySelector('#loginContainer').style.display ="block";
+    document.querySelector('#loggedInContainer').style.display ="none";
 };
